c252b6140a9810c21b294980aed315ea7515b7b4
delanosergeant <delano.sergeant@student.hogent.be>
2023-01-05 22:53:47 +0100

    init + add firebase

---
+++ Card unpacker/Card unpacker/Card_unpackerApp.swift
@@ -0,0 +1,33 @@
+//
+//  Card_unpackerApp.swift
+//  Card unpacker
+//
+//  Created by delano sergeant on 05/01/2023.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+
+
+
+@main
+struct Card_unpackerApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
